c042c2b43af1e9a34ff43eaea2987342f60cbfa4
Coelho <robertcoelho@live.com>
2013-04-20 14:50:21 -0700

    Ignore netty networking exceptions

--- src/main/java/lilypad/server/connect/node/NodeHandler.java
+++ src/main/java/lilypad/server/connect/node/NodeHandler.java
@@ -52,7 +52,7 @@ public class NodeHandler extends ChannelInboundMessageHandlerAdapter<Packet> {
 	
 	public void exceptionCaught(ChannelHandlerContext context, Throwable cause) throws Exception {
 		Channel channel = context.channel();
-		cause.printStackTrace();
+		//cause.printStackTrace(); ignore
 		if(channel.isOpen()) {
 			channel.close();
 		}
