766f46f4733c27386923e154c680fe18805495c2
Coelho <robertcoelho@live.com>
2013-05-21 17:42:52 -0700

    Be smarter and ignore bad packet ids

--- src/main/java/lilypad/packet/common/PacketDecoder.java
+++ src/main/java/lilypad/packet/common/PacketDecoder.java
@@ -8,13 +8,18 @@ import io.netty.handler.codec.ReplayingDecoder;
 public class PacketDecoder extends ReplayingDecoder<Void> {
 
 	private PacketCodecRegistry packetCodecRegistry;
-	
+
 	public PacketDecoder(PacketCodecRegistry packetCodecRegistry) {
 		this.packetCodecRegistry = packetCodecRegistry;
 	}
-	
+
 	public void decode(ChannelHandlerContext context, ByteBuf buffer, MessageBuf<Object> messageBuf) throws Exception {
-		messageBuf.add(this.packetCodecRegistry.getOpcode(buffer.readUnsignedByte()).decode(buffer));
+		PacketCodec<?> packetCodec = this.packetCodecRegistry.getOpcode(buffer.readUnsignedByte());
+		if(packetCodec == null) {
+			context.close();
+			return;
+		}
+		messageBuf.add(packetCodec.decode(buffer));
 	}
 
 }
