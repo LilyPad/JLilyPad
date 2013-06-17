053b1b8c0448eecb2c377f858d18cd326a1949d8
Coelho <robertcoelho@live.com>
2013-06-16 19:00:18 -0700

    Update to Netty 4 CR5

--- src/main/java/lilypad/server/connect/node/NodeHandler.java
+++ src/main/java/lilypad/server/connect/node/NodeHandler.java
@@ -11,48 +11,62 @@ import lilypad.server.connect.ConnectService;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundMessageHandlerAdapter;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.MessageList;
 import io.netty.handler.timeout.ReadTimeoutException;
 import io.netty.util.AttributeKey;
 
 @Sharable
-public class NodeHandler extends ChannelInboundMessageHandlerAdapter<Packet> {
+public class NodeHandler extends ChannelInboundHandlerAdapter {
 
 	private static final AttributeKey<NodeSession> nodeSession = new AttributeKey<NodeSession>("nodeSession");
-	
+
 	private ConnectService connectService;
 	private IAuthenticator authenticator;
 	private IPlayable playable;
-	
+
 	public NodeHandler(ConnectService connectService, IAuthenticator authenticator, IPlayable playable) {
 		this.connectService = connectService;
 		this.authenticator = authenticator;
 		this.playable = playable;
 	}
-	
+
+	@Override
 	public void channelActive(ChannelHandlerContext context) throws Exception {
 		context.attr(nodeSession).setIfAbsent(new NodeSession(context.channel(), this.connectService, this.authenticator, this.playable));
 	}
 
+	@Override
 	public void channelInactive(ChannelHandlerContext context) throws Exception {
 		context.attr(nodeSession).getAndSet(null).cleanup();
 	}
-	
-	public void messageReceived(ChannelHandlerContext context, Packet packet) throws Exception {
-		switch(packet.getOpcode()) {
-		case 0x00:
-			KeepalivePacket keepalivePacket = (KeepalivePacket) packet;
-			context.attr(nodeSession).get().pong(keepalivePacket.getRandom());
-			break;
-		case 0x01:
-			context.attr(nodeSession).get().handleRequest((RequestPacket) packet);
-			break;
-		default:
-			context.close(); // invalid packet
-			break;
+
+	@Override
+	public void messageReceived(ChannelHandlerContext context, MessageList<Object> msgs) throws Exception {
+		MessageList<Packet> packets = msgs.cast();
+		MessageList<Object> decodedPackets = MessageList.newInstance(); 
+		Packet packet;
+		for(int i = 0; i < msgs.size() && context.channel().isOpen(); i++) {
+			packet = packets.get(i);
+			switch(packet.getOpcode()) {
+			case 0x00:
+				KeepalivePacket keepalivePacket = (KeepalivePacket) packet;
+				context.attr(nodeSession).get().pong(keepalivePacket.getRandom());
+				break;
+			case 0x01:
+				context.attr(nodeSession).get().handleRequest((RequestPacket) packet);
+				break;
+			default:
+				context.close(); // invalid packet
+				break;
+			}
+			decodedPackets.add(packet);
 		}
+		packets.recycle();
+		context.fireMessageReceived(decodedPackets);
 	}
-	
+
+	@Override
 	public void exceptionCaught(ChannelHandlerContext context, Throwable cause) throws Exception {
 		Channel channel = context.channel();
 		if(cause instanceof IOException) {
