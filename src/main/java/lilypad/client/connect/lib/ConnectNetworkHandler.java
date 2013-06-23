25b68afe98fa9b6b11039efd3dcaea1829329a9c
Coelho <robertcoelho@live.com>
2013-06-23 10:54:28 -0700

    Change to SimpleChannelInboundHandler

--- src/main/java/lilypad/client/connect/lib/ConnectNetworkHandler.java
+++ src/main/java/lilypad/client/connect/lib/ConnectNetworkHandler.java
@@ -16,68 +16,61 @@ import lilypad.packet.connect.impl.ServerAddPacket;
 import lilypad.packet.connect.impl.ServerPacket;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.MessageList;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.timeout.ReadTimeoutException;
 
-public class ConnectNetworkHandler extends ChannelInboundHandlerAdapter {
+public class ConnectNetworkHandler extends SimpleChannelInboundHandler<Packet> {
 
 	private ConnectImpl connect;
 
 	public ConnectNetworkHandler(ConnectImpl connect) {
 		this.connect = connect;
 	}
-
+	
 	@Override
-	public void messageReceived(ChannelHandlerContext context, MessageList<Object> msgs) throws Exception {
-		MessageList<Packet> packets = msgs.cast();
-		Packet packet;
-		for(int i = 0; i < msgs.size() && context.channel().isOpen(); i++) {
-			packet = packets.get(i);
-			switch(packet.getOpcode()) {
-			case 0x00:
-				context.write(packet);
+	protected void messageReceived(ChannelHandlerContext context, Packet packet) throws Exception {
+		switch(packet.getOpcode()) {
+		case 0x00:
+			context.write(packet);
+			break;
+		case 0x02:
+			ResultPacket resultPacket = (ResultPacket) packet;
+			StatusCode statusCode;
+			switch(resultPacket.getStatusCode()) {
+			case ConnectPacketConstants.statusSuccess:
+				statusCode = StatusCode.SUCCESS;
 				break;
-			case 0x02:
-				ResultPacket resultPacket = (ResultPacket) packet;
-				StatusCode statusCode;
-				switch(resultPacket.getStatusCode()) {
-				case ConnectPacketConstants.statusSuccess:
-					statusCode = StatusCode.SUCCESS;
-					break;
-				case ConnectPacketConstants.statusInvalidRole:
-					statusCode = StatusCode.INVALID_ROLE;
-					break;
-				case ConnectPacketConstants.statusInvalidGeneric:
-				default:
-					statusCode = StatusCode.INVALID_GENERIC;
-					break;
-				}
-				this.connect.dispatchResult(resultPacket.getId(), statusCode, resultPacket.getPayload());
+			case ConnectPacketConstants.statusInvalidRole:
+				statusCode = StatusCode.INVALID_ROLE;
 				break;
-			case 0x03:
-				MessagePacket messagePacket = (MessagePacket) packet;
-				this.connect.dispatchMessageEvent(new MessageEvent(messagePacket.getSender(), messagePacket.getChannel(), messagePacket.getPayload().array()));
-				break;
-			case 0x04:
-				RedirectPacket redirectPacket = (RedirectPacket) packet;
-				this.connect.dispatchRedirectEvent(new RedirectEvent(redirectPacket.getServer(), redirectPacket.getPlayer()));
-				break;
-			case 0x05:
-				ServerPacket serverPacket = (ServerPacket) packet;
-				if(serverPacket.isAdding()) {
-					ServerAddPacket serverAddPacket = (ServerAddPacket) serverPacket;
-					this.connect.dispatchServerAddEvent(new ServerAddEvent(serverAddPacket.getServer(), serverAddPacket.getSecurityKey(), new InetSocketAddress(serverAddPacket.getAddress(), serverAddPacket.getPort())));
-				} else {
-					this.connect.dispatchServerRemoveEvent(serverPacket.getServer());
-				}
-				break;
+			case ConnectPacketConstants.statusInvalidGeneric:
 			default:
-				context.close(); // invalid packet
+				statusCode = StatusCode.INVALID_GENERIC;
 				break;
 			}
+			this.connect.dispatchResult(resultPacket.getId(), statusCode, resultPacket.getPayload());
+			break;
+		case 0x03:
+			MessagePacket messagePacket = (MessagePacket) packet;
+			this.connect.dispatchMessageEvent(new MessageEvent(messagePacket.getSender(), messagePacket.getChannel(), messagePacket.getPayload().array()));
+			break;
+		case 0x04:
+			RedirectPacket redirectPacket = (RedirectPacket) packet;
+			this.connect.dispatchRedirectEvent(new RedirectEvent(redirectPacket.getServer(), redirectPacket.getPlayer()));
+			break;
+		case 0x05:
+			ServerPacket serverPacket = (ServerPacket) packet;
+			if(serverPacket.isAdding()) {
+				ServerAddPacket serverAddPacket = (ServerAddPacket) serverPacket;
+				this.connect.dispatchServerAddEvent(new ServerAddEvent(serverAddPacket.getServer(), serverAddPacket.getSecurityKey(), new InetSocketAddress(serverAddPacket.getAddress(), serverAddPacket.getPort())));
+			} else {
+				this.connect.dispatchServerRemoveEvent(serverPacket.getServer());
+			}
+			break;
+		default:
+			context.close(); // invalid packet
+			break;
 		}
-		packets.releaseAllAndRecycle();
 	}
 
 	@Override
