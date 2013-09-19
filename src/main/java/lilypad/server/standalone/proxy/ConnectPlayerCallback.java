eed3aad6ddc443af7a4255e0087a01c612724b37
Coelho <me@iscoelho.com>
2013-09-18 21:33:12 -0700

    Fix concurrency safety issues regarding ConnectPlayerCallback

--- src/main/java/lilypad/server/standalone/proxy/ConnectPlayerCallback.java
+++ src/main/java/lilypad/server/standalone/proxy/ConnectPlayerCallback.java
@@ -2,6 +2,7 @@ package lilypad.server.standalone.proxy;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 
 import lilypad.client.connect.api.Connect;
 import lilypad.client.connect.api.request.RequestException;
@@ -16,16 +17,22 @@ public class ConnectPlayerCallback implements IPlayerCallback {
 
 	private Connect connect;
 	private Set<String> localPlayers = new HashSet<String>();
+	private ReentrantLock localPlayersLock = new ReentrantLock();
 	private int playerCount;
 	private int playerMaximum;
-	
+
 	public ConnectPlayerCallback(Connect connect) {
 		this.connect = connect;
 	}
-	
+
 	public void resendLocalPlayers() throws RequestException {		
-		for(String player : this.localPlayers) {
-			this.connect.request(new NotifyPlayerRequest(true, player));
+		this.localPlayersLock.lock();
+		try {
+			for(String player : this.localPlayers) {
+				this.connect.request(new NotifyPlayerRequest(true, player));
+			}
+		} finally {
+			this.localPlayersLock.unlock();
 		}
 	}
 
@@ -37,7 +44,12 @@ public class ConnectPlayerCallback implements IPlayerCallback {
 				return -1;
 			}
 			if(result.getStatusCode() == StatusCode.SUCCESS) {
-				this.localPlayers.add(player);
+				this.localPlayersLock.lock();
+				try {
+					this.localPlayers.add(player);
+				} finally {
+					this.localPlayersLock.unlock();
+				}
 				return 1;
 			}
 			return 0;
@@ -55,9 +67,14 @@ public class ConnectPlayerCallback implements IPlayerCallback {
 		} catch(RequestException exception) {
 			// ignore
 		}
-		this.localPlayers.remove(player);
+		this.localPlayersLock.lock();
+		try {
+			this.localPlayers.remove(player);
+		} finally {
+			this.localPlayersLock.unlock();
+		}
 	}
-	
+
 	public boolean queryPlayers() throws RequestException, InterruptedException {
 		GetPlayersResult result = this.connect.request(new GetPlayersRequest(false)).await(10000L);
 		if(result == null) {
@@ -67,15 +84,15 @@ public class ConnectPlayerCallback implements IPlayerCallback {
 		this.playerMaximum = result.getMaximumPlayers();
 		return true;
 	}
-	
+
 	public int getPlayerCount() {
 		return this.playerCount;
 	}
-	
+
 	public int getPlayerMaximum() {
 		return this.playerMaximum;
 	}
-	
+
 	public void clearPlayers() {
 		this.playerCount = 0;
 		this.playerMaximum = 0;
