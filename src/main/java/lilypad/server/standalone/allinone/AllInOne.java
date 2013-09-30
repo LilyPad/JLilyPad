92003d78700a1e1a6b280449332080788643b7fa
boboman13 <boboman13@users.noreply.github.com>
2013-09-30 09:22:33 -0700

    Remove comment and add halt
    Adds 'halt' to the request, so 'stop' and 'halt' both work. Changes .equals() to .equalsIgnoreCase().

--- src/main/java/lilypad/server/standalone/allinone/AllInOne.java
+++ src/main/java/lilypad/server/standalone/allinone/AllInOne.java
@@ -45,8 +45,7 @@ public class AllInOne {
 			try {
 				String scannerLine;
 				while((scannerLine = scanner.next()) != null) {
-					/* These are the commands */
-					if(scannerLine.equals("stop")) {
+					if(scannerLine.equalsIgnoreCase("stop") || scannerLine.equalsIgnoreCase("halt")) {
 						break;
 					} else {
 						System.out.println("Command not recognized.");
